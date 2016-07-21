5f2f80a7ada9c43795762e4952b7876c6e8e4c55
leonel exequiel gomez <lgomez@andaman.inria.fr>
2016-07-21 17:58:13 +0200

    cambiando el metodo que normaliza una matriz

--- utils/math_utils.py
+++ utils/math_utils.py
@@ -6,22 +6,25 @@ def normalize_by_bval(m):
         O podria devolver muchos NaNs
         return m/||m||
     """
+    maxmax=-1
     res = np.ones(m.shape)
     for i in range(0, m.shape[3]):
         layer = m[:,:,:,i]
-        layer_norm = np.linalg.norm(layer)
-        val = layer/np.linalg.norm(layer)
+        layer_max = np.max(layer)
+        maxmax = layer_max if layer_max > maxmax else maxmax
+        val = layer/layer_max
         res[:,:,:,i] = val
+    print 'maximo valor antes de normalizar :', maxmax
     return res
 
 
 def mse_by_bval(m1,m2):
-    dif_pow2 = (m2 - m1)**2
     means = []
-    for i in range(0, dif_pow2.shape[3]):
-        means.append(dif_pow2[:,:,:,i].mean())
+    for i in range(0, m1.shape[3]):
+        dif_pow2 = np.power((m2[:,:,:,i] - m1[:,:,:,i]), 2)
+        means.append(dif_pow2.mean())
     return np.array(means, dtype=float)
 
 def normalize(matrix):
-    norm = np.linalg.norm(matrix)
-    return matrix/norm
+    #norm = np.linalg.norm(matrix)
+    return matrix/np.max(matrix)
